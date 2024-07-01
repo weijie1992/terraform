3dc7b794906e90a97045965bd4143a56e8bc1b77
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-02 00:38:11 +0800

    create vpc, subnet, ec2, security group

---
+++ 06-resources/README.md
@@ -0,0 +1,9 @@
+Steps for implementation:
+
+1. [DONE] Deploy a VPC and a subnet
+2. [DONE] Depoy an internet gateway and associate it with the VPC
+3. [DONE] Setup a route table with a route to the IGW
+4. [DONE] Deploy an EC2 instance inside of the created subnet
+5. [DONE] Associate a public IP and a SG that allows public ingress
+6. Change the EC2 instance to use a publicly available NGINX AMI
+7. Destroy everything
---
+++ 06-resources/compute.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "web" {
+  ami                         = "ami-0417ea7f58950ec35"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
+  root_block_device { //not required
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-ec2"
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "06-resources-sg"
+  })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
