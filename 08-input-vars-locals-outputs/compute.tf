048e7a4d94eef3b4f75a08d36895c33c49bd6a7b
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-07 01:09:45 +0800

    variable with default value, validation  and non-default value

---
+++ 08-input-vars-locals-outputs/compute.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] #Owner is Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.weijie-vpc.id
+}
+
+resource "aws_instance" "compute" {
+  vpc_security_group_ids = [aws_security_group.public_http_traffic.id]
+  subnet_id              = aws_subnet.weijie-public-subnet.id
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.ec2_instance_type
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = var.ec2_volume_size
+    volume_type           = var.ec2_volume_type
+  }
+}
---
+++ 08-input-vars-locals-outputs/variables.tf
@@ -0,0 +1,23 @@
+variable "ec2_instance_type" {
+  type    = string
+  default = "t2.micro"
+
+  validation {
+    # condition     = var.ec2_instance_type == "t2.micro" || var.ec2_instance_type == "t3.micro"
+    condition     = contains(["t2.micro", "t3.micro"], var.ec2_instance_type)
+    error_message = "Only supports t2.micro and t3.micro"
+  }
+  description = "The size of manage EC2 instance"
+}
+
+variable "ec2_volume_size" {
+  type        = number
+  description = "The size of the root block volume attached to managed EC2 instances"
+}
+
+variable "ec2_volume_type" {
+  type        = string
+  description = "The volume type between gp2 and gp3."
+}
+
+
