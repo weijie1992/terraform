4a6f4f7345eb26dc8f064344d4ee6bbc4e3e06c9
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-06 23:46:32 +0800

    use datasource for iam policy, the output iam_policy will return a json format of policy

--- 07-data-sources/compute.tf
+++ 07-data-sources/compute.tf
@@ -44,9 +44,31 @@ output "subnet_count" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
 output "azs" {
   value = data.aws_availability_zones.available
+}
+
+data "aws_iam_policy_document" "static_website" {
+  statement {
+    sid = "PublicReadGetObject"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.public_read_bucket.arn}/*"]
+    # resources = ["arn:aws:s3:::*/*"
+  }
+}
+
+resource "aws_s3_bucket" "public_read_bucket" {
+  bucket = "weijie-public-read-bucket"
+}
+
+output "iam_policy" {
+  value = data.aws_iam_policy_document.static_website.json
 }
 
 resource "aws_instance" "web" {
