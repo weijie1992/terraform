accb915c563d6c1f67f6aa6a62723c55b0156100
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-14 13:46:26 +0800

    create subnet using count

---
+++ 11-multiple-resources/networking.tf
@@ -0,0 +1,23 @@
+locals {
+  project = "11-multiple-resources"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Project = local.project
+    Name    = local.project
+  }
+}
+
+resource "aws_subnet" "main" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.${count.index * 16}/28"
+
+  tags = {
+    Project = local.project
+    Name    = "${local.project}-${count.index}"
+  }
+}
