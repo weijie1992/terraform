0175a8e75e0ea86ff931c3543afa9b48c54022c2
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-17 00:04:47 +0800

    add map validation

--- 11-multiple-resources/variables.tf
+++ 11-multiple-resources/variables.tf
@@ -29,4 +29,12 @@ variable "ec2_instance_config_map" {
     ami           = string
     subnet_index  = optional(number, 0)
   }))
+  validation {
+    condition     = alltrue([for config in values(var.ec2_instance_config_map) : contains(["t2.micro"], config.instance_type)])
+    error_message = "Only t2.micro is allow"
+  }
+  validation {
+    condition     = alltrue([for config in values(var.ec2_instance_config_map) : contains(["ubuntu", "nginx"], config.ami)])
+    error_message = "Only Ubuntu and Nginx ami are allow"
+  }
 }
