4f310faf8508aa47768861e979d2ca625ee1d764
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-20 22:54:40 +0800

    add data for aws_iam_policy, attached created role such as readonly, admin, auditor, developer to the data policy

--- proj02-iam-users/roles.tf
+++ proj02-iam-users/roles.tf
@@ -15,8 +15,22 @@ locals {
       "AmazonRDSFullAccess"
     ]
   }
-}
 
+  role_policies_list = flatten([
+    for role, policies in local.role_policies : [
+      for policy in policies : {
+        role   = role
+        policy = policy
+      }
+    ]
+  ])
+}
+output "role_policies_list" {
+  value = local.role_policies_list
+}
+output "aws_iam_role" {
+  value = aws_iam_role.roles
+}
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -32,5 +46,15 @@ resource "aws_iam_role" "roles" {
   for_each           = toset(keys(local.role_policies))
   name               = each.key
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
 
+data "aws_iam_policy" "managed_policies" {
+  for_each = toset(local.role_policies_list[*].policy)
+  arn      = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachments" {
+  count      = length(local.role_policies_list)
+  role       = aws_iam_role.roles[local.role_policies_list[count.index].role].name
+  policy_arn = data.aws_iam_policy.managed_policies[local.role_policies_list[count.index].policy].arn
 }
