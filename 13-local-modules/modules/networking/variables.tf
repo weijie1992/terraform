a4582881fd0dd511008fb937b10e5b9e54912617
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-07-21 15:29:18 +0800

    use object instead of variable for vpc configs,create aws vpc resource

--- 13-local-modules/modules/networking/variables.tf
+++ 13-local-modules/modules/networking/variables.tf
@@ -1,8 +1,11 @@
-variable "vpc_cidr" {
-  type = string
+variable "vpc_config" {
+  type = object({
+    cidr_block = string
+    name       = string
+  })
 
   validation {
-    condition     = can(cidrnetmask(var.vpc_cidr))
-    error_message = "The variable vpc_cidr must contain a valid CIDR block."
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
+    error_message = "The cidr_block config option must contain a valid CIDR block."
   }
 }
--- 13-local-modules/networking.tf
+++ 13-local-modules/networking.tf
@@ -1,4 +1,7 @@
 module "networking" {
-  source   = "./modules/networking"
-  vpc_cidr = "10.0.0.0/24"
+  source = "./modules/networking"
+  vpc_config = {
+    cidr_block = "10.0.0.0/24"
+    name       = "13-local-modules"
+  }
 }
