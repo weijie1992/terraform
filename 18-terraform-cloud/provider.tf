cb5cc8d75cc3741ff1bb898915e940bc202826ef
PhuaWeiJie <wei_jie_phua_from.tp@mom.gov.sg>
2024-08-10 21:47:00 +0800

    Understand terraform cloud - init with terraform cloud

---
+++ 18-terraform-cloud/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "weijie-test"
+    workspaces {
+      name = "terraform-cli"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
